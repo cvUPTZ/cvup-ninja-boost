91f519c0127e4ac1dfb3f09488f595c30191211f
cvUPTZ <cvupdz@gmail.com>
2024-12-19 15:07:25 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 const root = document.getElementById('root');
 
