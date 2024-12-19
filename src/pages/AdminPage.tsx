7ef0b0f2cbdf00f487465cdda0a0dbd6b4cf2502
cvUPTZ <cvupdz@gmail.com>
2024-12-19 15:09:00 +0100

    Create AdminPage.tsx

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,15 @@
+// src/pages/AdminPage.tsx
+
+import React from "react";
+
+const AdminPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Here you can see the statistics and manage users.</p>
+      {/* Add your statistics and other admin features here */}
+    </div>
+  );
+};
+
+export default AdminPage;
